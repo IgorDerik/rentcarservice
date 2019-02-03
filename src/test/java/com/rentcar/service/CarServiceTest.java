47bde13b584afc4d9188dc189738d58c8b06f357
Igor Dereviankin <Igor_Dereviankin@epam.com>
2019-02-04 00:00:55 +0200

    added some skeletons for tests

---
+++ src/test/java/com/rentcar/service/CarServiceTest.java
@@ -0,0 +1,7 @@
+package java.com.rentcar.service;
+
+import static org.junit.Assert.*;
+
+public class CarServiceTest {
+
+}
---
+++ src/test/java/com/rentcar/web/CarControllerTest.java
@@ -0,0 +1,28 @@
+package java.com.rentcar.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarControllerTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void setBookingStatus() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getAvailable() {
+    }
+
+    @Test
+    public void getCarByUid() {
+    }
+}
